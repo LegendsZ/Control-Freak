bde05e5a0628e5e171b1c6760378265281d7242f
LegendsZ <akkshai@yahoo.com>
2022-03-29 18:26:22 -0400

    basic comm outline

---
+++ GameClient/ControlFreak-Game/ControlFreak-Game/main.cpp
@@ -0,0 +1,103 @@
+/*
+	Authors: Akkshai. B, Roland. T
+	Date creation: 3/6/2022
+	Version: 1.0.0.0
+*/
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <iostream> //debugging/testing purposes
+#include "Client/client.h"
+
+
+struct IPv4
+{
+	unsigned char b1, b2, b3, b4;
+};
+
+bool getMyIP(IPv4& myIP)
+{
+	char szBuffer[1024];
+
+#ifdef WIN32
+	WSADATA wsaData;
+	WORD wVersionRequested = MAKEWORD(2, 0);
+	if (::WSAStartup(wVersionRequested, &wsaData) != 0)
+		return false;
+#endif
+
+
+	if (gethostname(szBuffer, sizeof(szBuffer)) == SOCKET_ERROR)
+	{
+#ifdef WIN32
+		WSACleanup();
+#endif
+		return false;
+	}
+
+	struct hostent* host = gethostbyname(szBuffer);
+	if (host == NULL)
+	{
+#ifdef WIN32
+		WSACleanup();
+#endif
+		return false;
+	}
+
+	//Obtain the computer's IP
+	myIP.b1 = ((struct in_addr*)(host->h_addr))->S_un.S_un_b.s_b1;
+	myIP.b2 = ((struct in_addr*)(host->h_addr))->S_un.S_un_b.s_b2;
+	myIP.b3 = ((struct in_addr*)(host->h_addr))->S_un.S_un_b.s_b3;
+	myIP.b4 = ((struct in_addr*)(host->h_addr))->S_un.S_un_b.s_b4;
+
+#ifdef WIN32
+	WSACleanup();
+#endif
+	return true;
+}
+
+
+
+int main() {
+	system("title CLIENT");
+
+
+	std::string ipG;
+	std::cout << "IP: ";
+	std::getline(std::cin, ipG);
+
+	std::string portG;
+	std::cout << "PORT: ";
+	std::getline(std::cin, portG);
+
+	client* clientOBJ = new client(ipG, std::stoi(portG));
+
+	if (!clientOBJ->autoStart()) {
+		return 0; //error handle?
+	}
+	system("pause");
+	std::string myIPV4 = "";
+	IPv4 myIPV4TEMP;
+	getMyIP(myIPV4TEMP);
+	myIPV4 = std::to_string(myIPV4TEMP.b1) + "." + std::to_string(myIPV4TEMP.b2) + "." + std::to_string(myIPV4TEMP.b3) + "." + std::to_string(myIPV4TEMP.b4);
+	
+	clientOBJ->sendData(myIPV4 + "ISREADY");
+
+	system("pause");
+
+	while (true) {
+		if (clientOBJ->recieved.size() > 0) {
+			if (clientOBJ->recieved.back() == "") { //check for whatever data input
+
+				clientOBJ->recieved.pop_back();
+			}
+		}
+	}
+
+	system("pause");
+	//code below will tell server i am dc'ing
+	clientOBJ->sendData(myIPV4 + "ISDCING");
+	//garbage collections
+	clientOBJ->Terminate();
+
+	return 0;
+}
--- GameServer/ControlFreak-Server/ControlFreak-Server/main.cpp
+++ GameServer/ControlFreak-Server/ControlFreak-Server/main.cpp
@@ -3,75 +3,75 @@
 	Date creation: 3/6/2022
 	Version: 1.0.0.0
 */
-
 #include <iostream> //debugging/testing purposes
-
-#include "ServerClient/client.h"
-#include "ServerClient/server.h"
+#include "Server/server.h"
 
 
-
-//FOR PRINTING TO SERVER
-void FprintStuffS(bool* updateScreenG, server* serverG) {
-	while (*updateScreenG) {
-		if (serverG->recieved.size() != 0) {
-			std::cout << "Console: " << serverG->recieved.front() << "\n> ";
-			//serverG->recieved.erase(serverG->recieved.begin()); //dont remove for now
-		}
-	}
-}
-
-
-
-//ENTRY POINT
-int main() {
+int main()
+{
 	system("title SERVER");
-
-	server* serverOBJ = nullptr;
-
-	//BELOW CODE IS FOR SERVER INITIALIZATION
-	std::cout << "Enter port number: ";
-	std::string input;
-	std::getline(std::cin,input);
-	serverOBJ = new server(std::stoi(input));
-	if (!serverOBJ->autoStart()) {
-		return 0;
-	}
-	std::cout << "Created server on port [" << input << "]!\n";
-	std::cout << "Once you are done accepting, ";
 	
-	while (serverOBJ->getConnectionsCount() < 2) {
-		system("cls");
-		system("pause");
+	std::string portG;
+	std::cout << "PORT: ";
+	std::getline(std::cin, portG);
+
+	server* serverOBJ = new server(std::stoi(portG));
+	
+
+	if (!serverOBJ->autoStart()) {
+		return 0; //error handle this
 	}
-	system("cls");
-
-	std::cout << "Stopped accepting clients!\n\n";
-	serverOBJ->endFindConnections();
-	serverOBJ->startListening();
-	bool* updateScreen = new bool;
-	//std::thread updateScreenThread(FprintStuffS, updateScreen, serverOBJ); //keep commented for now
-	//ABOVE CODE IS FOR SERVER INITIALIZATION
-
-	if (!serverOBJ->sendData(-1, "READYTOSTART")) {
-		std::cout << "Failed to send message!";
-		return 0; //? idk how to error handle this
-	}
-
-	//figure out how to check if all players are ready
-	//
-	while (true) {
-		std::cout << "Console: " << serverOBJ->recieved.front() << "\n> ";
-		std::string first = serverOBJ->recieved.front();
-		serverOBJ->recieved.erase(serverOBJ->recieved.begin());
-		//if (first == "STARTGAME")
-	}
-
-
-
+	std::cout << "Once you are done accepting, ";
 	system("pause");
-	*updateScreen = false;
-	serverOBJ->Terminate(); //garbage collec / termination
+	serverOBJ->endFindConnections();
+	std::cout << "Stopped accepting clients!\n\n";
+
+	serverOBJ->startListening();
+
+	bool allPlayersReady = false;
+	int* readyPlayers = new int();
+	*readyPlayers = 0;
+	while (serverOBJ->getConnectionsCount() > 0 && !allPlayersReady) {
+		if (serverOBJ->recieved.size() > 0) {
+			if (serverOBJ->recieved.back().find("ISREADY") != std::string::npos) {
+				std::string tempIP = serverOBJ->recieved.back().substr(0, serverOBJ->recieved.back().size() - 8);
+				for (int i = 0; i < serverOBJ->playerList.size(); i++) {
+					if (serverOBJ->playerList[i]->getIP() == tempIP && !serverOBJ->playerList[i]->isReady()) {
+						serverOBJ->playerList[i]->setReady(true);
+						*readyPlayers = *readyPlayers + 1;
+						break;
+					}
+				}
+				serverOBJ->recieved.pop_back();
+				if (*readyPlayers == serverOBJ->playerList.size()) {
+					break;
+				}
+			}
+		}
+		//maybe a sleep?
+	}
+
+
+	while (serverOBJ->getConnectionsCount() > 0) {
+		if (serverOBJ->recieved.size() > 0) {
+			if (serverOBJ->recieved.back().find("ISDCING")) {
+				std::string tempIP = serverOBJ->recieved.back().substr(0, serverOBJ->recieved.back().size() - 8);
+				for (int i = 0; i < serverOBJ->playerList.size(); i++) {
+					if (serverOBJ->playerList[i]->getIP() == tempIP) {
+						serverOBJ->playerList.erase(serverOBJ->playerList.begin() + i);
+						break;
+					}
+				}
+			}
+		}
+		//maybe a sleep?
+	}
+	
+	system("pause");
+	
+	//garbage collection
+	serverOBJ->Terminate();
 	delete serverOBJ;
-	delete updateScreen;
+
+	return 0;
 }
